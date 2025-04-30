c82d5dd4739c06c3361e837cefa49229c5ee51fa
Muhammad-Harris55 <Harrisimran55@gmail.com>
2025-04-30 06:50:53 -0700

    Harris Stagger animation added

--- src/Components/HoverImageMobile.jsx
+++ src/Components/HoverImageMobile.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import "../Styles/hover-image-mobile.css"
+import React, { useEffect, useRef } from 'react';
+import "../Styles/hover-image-mobile.css";
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const HoverImageMobile = () => {
+  const containerRef = useRef(null);
+
+  useGSAP(() => {
+    const items = containerRef.current.querySelectorAll('.hover-image-mobile-item');
+
+    items.forEach((item) => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: item, // Trigger animation for each individual card
+          start: 'top 80%', // Start animation when the card enters the viewport
+          end: 'top 50%', // End animation when the card is halfway in the viewport
+          scrub: 1,
+          // markers: true, // Enable markers for debugging
+        },
+      });
+
+      tl.from(item, {
+        y: 100,
+        opacity: 0,
+        duration: 0.8,
+        ease: 'power3.inOut',
+      });
+    });
+  });
+
   return (
     <>
-      <div className="hover-image-mobile-container">
+      <div className="hover-image-mobile-container" ref={containerRef}>
         <div className="hover-image-mobile-item">
           <img
             src="/Assets/hm1.jpg"
@@ -15,18 +42,18 @@ const HoverImageMobile = () => {
             <p>Welcome To Jaikor GP, A Leading Name In Construction Company, Dedicated To Delivering Exceptional Infrastructure Solutions</p>
           </div>
         </div>
-        
+
         <div className="hover-image-mobile-item">
-          <img 
-            src="/Assets/hm2.jpg" 
-            alt="Urban skyscraper at dusk" 
+          <img
+            src="/Assets/hm2.jpg"
+            alt="Urban skyscraper at dusk"
           />
           <div className="hover-image-mobile-text">
             <h2>Lorem Ipsum Dolor Sit</h2>
             <p>Welcome To Jaikor GP, A Leading Name In Construction Company, Dedicated To Delivering Exceptional Infrastructure Solutions</p>
           </div>
         </div>
-        
+
         <div className="hover-image-mobile-item">
           <img
             src="/Assets/hm3.jpg"
@@ -37,7 +64,7 @@ const HoverImageMobile = () => {
             <p>Welcome To Jaikor GP, A Leading Name In Construction Company, Dedicated To Delivering Exceptional Infrastructure Solutions</p>
           </div>
         </div>
-        
+
         <div className="hover-image-mobile-item">
           <img
             src="/Assets/carousel-img.png"
@@ -50,7 +77,7 @@ const HoverImageMobile = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default HoverImageMobile
+export default HoverImageMobile;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,3 +38,4 @@ const Home = ({ lenis }) => {
 };
 
 export default Home;
+
